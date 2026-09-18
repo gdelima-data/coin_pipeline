38dd705cee1ef54fcab9f58feb817715a0418c59
gdelima-data <gabriel04121922@outlook.com>
2026-09-18 01:43:45 +0000

    Atualização no ingest

--- dags/scripts/ingest.py
+++ dags/scripts/ingest.py
@@ -3,8 +3,39 @@ import os
 from clickhouse_driver import Client
 
 def fetch_and_load_api():
-    url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1'
-    response = requests.get(url).json()
+    url = 'https://api.coingecko.com/api/v3/coins/markets'
+
+    params = {
+      'vs_currency': 'usd',
+      'order': 'market_cap_desc',
+      'per_page': 250,
+      'page': 1
+    }
+
+    all_data = []
+
+    while True:
+	response = requests.get(
+	base_url,
+	params=params,
+	timeout=30
+	)
+
+        data = response.json()
+
+        if not data:
+	     break
+
+         all_data.extend(data)
+
+         print(f'Página {params['page']} coletada: '
+         f'{len(data)} registros'
+         )
+
+         params['page'] += 1
+     
+    print(f'Total coletado: {len(all_data)} registros')
+
 
     client = Client(host='clickhouse', user='admin', password=os.environ['CLICKHOUSE_PASSWORD'], database='crypto_db')
 
